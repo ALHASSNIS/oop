43753fb45716d981ea5af9d7af8604c8c9168093
الحسني <112685728+ALHASSNIS@users.noreply.github.com>
2022-10-05 15:03:48 +0300

    Add files via upload

---
+++ h1_2.java
@@ -0,0 +1,12 @@
+/**
+ * Created by H on 14/09/2022.
+ */
+public class h1_2 {
+    public static void main(String[] args) {
+        int i,j=0;
+        for (i=1;i<=10;i++){
+            j=j+i;
+        }
+        System.out.println(j);
+    }
+}
